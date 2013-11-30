15c93adaec7168e0f47a0a639275e61cc31a61a6
Fravic <fravicf@gmail.com>
2013-11-29 17:26:15 -0800

    Add map view and server

--- PhotosNearby/PNPhotoListViewCell.m
+++ PhotosNearby/PNPhotoListViewCell.m
@@ -7,9 +7,11 @@
 //
 
 #import "PNPhotoListViewCell.h"
+#import <MapKit/MapKit.h>
 
 @implementation PNPhotoListViewCell {
     PNPhoto *_photo;
+    MKMapView *_mapView;
 }
 
 - (void)setPhoto:(PNPhoto*)photo {
@@ -32,6 +34,12 @@
     CGRect imageFrame = CGRectMake(PN_PHOTO_LIST_VIEW_CELL_H_PAD/2, PN_PHOTO_LIST_VIEW_CELL_V_PAD/2, imageWidth,
         _photo.height/_photo.width * imageWidth);
     self.imageView.frame = imageFrame;
+    
+    if (_mapView) {
+        CGRect mapFrame = CGRectMake(PN_PHOTO_LIST_VIEW_CELL_H_PAD/2, PN_PHOTO_LIST_VIEW_CELL_V_PAD/2, imageWidth,
+            _photo.height/_photo.width * imageWidth);
+        _mapView.frame = mapFrame;
+    }
 }
 
 - (void)setActive:(BOOL)active {
@@ -42,12 +50,52 @@
     }
 }
 
+- (void)addMapView {
+    _mapView = [[MKMapView alloc] init];
+    _mapView.alpha = 0.0f;
+    [self addSubview:_mapView];
+    [self layoutSubviews];
+}
+
+- (void)removeMapView {
+    [_mapView removeFromSuperview];
+}
+
 - (void)animateToActiveState {
+    [self addMapView];
     
+    CGFloat imageWidth = self.bounds.size.width - PN_PHOTO_LIST_VIEW_CELL_H_PAD;
+    CGRect activeFrame = CGRectMake(0, PN_PHOTO_LIST_VIEW_CELL_V_PAD/2, imageWidth,
+                                    _photo.height/_photo.width * imageWidth);
+
+    [UIView animateWithDuration:0.25f
+                     animations:^{
+                         self.imageView.frame = activeFrame;
+                         _mapView.alpha = 1.0f;
+                     }
+                     completion:^(BOOL finished){
+                     }
+     ];
+    
+    [UIView beginAnimations:@"reposition" context:NULL];
+
+    [UIView commitAnimations];
 }
 
 - (void)animateToDefaultState {
-    
+    CGFloat imageWidth = self.bounds.size.width - PN_PHOTO_LIST_VIEW_CELL_H_PAD;
+    CGRect inactiveFrame = CGRectMake(PN_PHOTO_LIST_VIEW_CELL_H_PAD/2, PN_PHOTO_LIST_VIEW_CELL_V_PAD/2, imageWidth,
+                                    _photo.height/_photo.width * imageWidth);
+
+    [UIView animateWithDuration:0.25f
+                     animations:^{
+                         self.imageView.frame = inactiveFrame;
+                         _mapView.alpha = 0.0f;
+                     }
+                     completion:^(BOOL finished){
+                         [self removeMapView];
+                     }
+     ];
 }
 
 @end
