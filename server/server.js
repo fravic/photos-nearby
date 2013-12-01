848df5f36b1f51a4737717a8f4cbe19594c20835
Fravic <fravicf@gmail.com>
2013-11-30 16:05:27 -0800

    ADD server/data endpoint

--- server/server.js
+++ server/server.js
@@ -6,10 +6,12 @@ app.use(express.logger());
 
 var API_URL_HOST = "api.500px.com";
 var PHOTO_SEARCH_PATH = "/v1/photos/search";
+var PHOTO_DATA_PATH = "/v1/photos";
 var CATEGORIES = "Landscapes,City%20and%20Architecture";
 var CONSUMER_KEY = process.env.CONSUMER_KEY;
 var PAGE_SIZE = 20;
 
+// Returns the photo list for a given coordinate
 app.get('/', function(req, res) {
     var lat = req.query.lat;
     var lng = req.query.lng;
@@ -43,7 +45,7 @@ app.get('/', function(req, res) {
                  function(statusCode, result) {
                      res.statusCode = statusCode;
 
-                     if (statusCode != 500) {
+                     if (statusCode == 200) {
                          var photoList = [];
                          result.photos.forEach(function(p) {
                              var photo = {};
@@ -59,6 +61,60 @@ app.get('/', function(req, res) {
                      }
 
                  });
+});
+
+// Returns additional secondary data for a list of photos
+app.get('/data', function(req, res) {
+    var photoIds = req.query.photos;
+
+    if (!photoIds) {
+        res.send(500, { error: 'Must provide a list of photos!' });
+        return false;
+    }
+    photoIds = photoIds.split(",");
+    var photos = [];
+
+    res.statusCode = 200;
+
+    photoIds.forEach(function(pid) {
+        var path = PHOTO_DATA_PATH;
+        path += "/" + pid;
+        path += "?consumer_key=" + CONSUMER_KEY;
+
+        var options = {
+            host: API_URL_HOST,
+            https: true,
+            path: path,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        rest.getJSON(options,
+                     function(statusCode, result) {
+                         if (res.statusCode != 200) {
+                             // Should have already send response by this point
+                             return false;
+                         }
+
+                         res.statusCode = statusCode;
+
+                         if (statusCode == 200) {
+                             var photo = {};
+                             var properties = ["id", "focal_length", "iso", "shutter_speed", "aperture", "taken_at"];
+                             properties.forEach(function(prop) {
+                                 photo[prop] = result.photo[prop];
+                             });
+                             photos.push(photo);
+
+                             if (photos.length == photoIds.length) {
+                                 res.send(JSON.stringify(photos));
+                             }
+                         } else {
+                             res.send({apiError: result});
+                         }
+                     });
+    });
 });
 
 var port = process.env.PORT || 8080;
